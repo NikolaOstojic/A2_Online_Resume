095fe265bf3be2569738c997b5f020aa8c2f83dc
Nikola Ostojic <nikola.520@hotmail.com>
2015-07-14 17:33:54 -0400

    save

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Nikola";
+var role = "Software Develloper";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+    finalName = finalName.toLowerCase();
+
+
+
+    // Don't delete this line!
+    return finalName;
+};
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
